f9645a58bccd45393b29427268e174416787b831
BARREIS3 <isobel.barrett_ext@novartis.com>
2022-06-20 17:59:51 +0000

    Upload New File

---
+++ man-roxygen/formula.R
@@ -0,0 +1,2 @@
+#' @param formula Formula object. The response (on the left of the `~` operator) must be a survival object as returned by the `Surv` function.
+#' The terms (on the right of the `~` operator) must include the treatment arm indicator, and additionally can include strata using the `strata` function.
