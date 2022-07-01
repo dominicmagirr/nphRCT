db05ba2dd4607690d62eb2f8352287157ef64fce
Barrett <barreis3@glchbs-sp220447.eu.novartis.net>
2022-07-01 13:30:59 +0200

    find_weights returns vector

--- tests/testthat/test-find_weights.R
+++ tests/testthat/test-find_weights.R
@@ -9,67 +9,49 @@ sim_data <- sim_events_delay(
   rec_power = 1,
   max_cal_t = 36
 )
-df_weights_mw<-find_weights(formula=Surv(event_time,event_status)~group,
+weights_mw<-find_weights(formula=Surv(event_time,event_status)~group,
                             data=sim_data,
                             wlr="mw",
                             t_star = 4
 )
-df_weights_mw_s_star<-find_weights(formula=Surv(event_time,event_status)~group,
+weights_mw_s_star<-find_weights(formula=Surv(event_time,event_status)~group,
                                    data=sim_data,
                                    wlr="mw",
                                    s_star = 0.5
 )
-df_weights_mw_0<-find_weights(formula=Surv(event_time,event_status)~group,
+weights_mw_0<-find_weights(formula=Surv(event_time,event_status)~group,
                               data=sim_data,
                               wlr="mw",
                               t_star = 0
 )
-df_weights_cens<-find_weights(formula=Surv(event_time,event_status)~group,
+weights_cens<-find_weights(formula=Surv(event_time,event_status)~group,
                               data=sim_data,
                               wlr="mw",
                               t_star = 0,
                               include_cens=FALSE
 )
-df_weights_lr<-find_weights(formula=Surv(event_time,event_status)~group,
+weights_lr<-find_weights(formula=Surv(event_time,event_status)~group,
                          data=sim_data,
                          wlr="lr"
 )
-df_weights_fh<-find_weights(formula=Surv(event_time,event_status)~group,
+weights_fh<-find_weights(formula=Surv(event_time,event_status)~group,
                          data=sim_data,
                          wlr="fh",
                          rho = 0,
                          gamma=1
 )
 
-for (df_weights in 
-     list(df_weights_mw,df_weights_lr,df_weights_fh,
-          df_weights_mw_s_star,df_weights_mw_0,df_weights_cens)){
-  test_that("returns a data frame", {expect_s3_class(df_weights, "data.frame")})
-  
-  test_that("correct number of rows", {
-    expect_true(nrow(df_weights)<=2000)
-  })
-  
-  test_that("check time is strictly increasing", {
-    expect_true(all(diff(df_weights$t_j)>0))
-  })
-  
-  
-  test_that("positive number of events", {
-    expect_true(all(df_weights$n_event>=0))
-  })
-  
-  test_that("positive number of censored", {
-    expect_true(all(df_weights$S_hat<=df_weights$S_hat_minus))
+for (weights in 
+     list(weights_mw,weights_lr,weights_fh,
+          weights_mw_s_star,weights_mw_0,weights_cens)){
+  test_that("correct length", {
+    expect_true(length(weights)<=2000)
   })
   
   test_that("weights are increasing", {
-    expect_true(all(diff(df_weights$w)>=0))
+    expect_true(all(diff(weights)>=0))
   })
   
-  test_that("number at risk are decreasing", {
-    expect_true(all(diff(df_weights$n_risk)<0))
-  })
 }
 
 #Test errors
