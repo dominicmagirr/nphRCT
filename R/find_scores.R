03ed2c3e36ca969ba153cee5250b12c267f6957a
Barrett <barreis3@glchbs-sp220444.eu.novartis.net>
2022-07-04 11:08:15 +0200

    -Update description -Simplify method -Add transparent points

--- R/find_scores.R
+++ R/find_scores.R
@@ -13,11 +13,21 @@
 #' @template s_star
 #' @template rho
 #' @template gamma
-#' @return Data frame containing output from function `find_weights` and also additional columns:
-#' 
+#' @return 
+#' Data frame. Each row corresponds to an event time (including censoring times if `include_cens=TRUE`).
+#' At each time specified in `t_j` the columns indicate
 #' - `score_censored` the scores for each censoring at time `t_j` in the permutation test formulation
 #' - `score_event` the scores for each event at time `t_j` in the permutation test formulation
-#' - `rank` the rank of the ordered event and censoring times, the higher the number the earlier the time `t_j`
+#' - `event` the event indicator
+#' - `group` the treatment arm indicator
+#' - `score` the value of the score for this individual, equal to `score_censored` if the individual is censored 
+#' and equal to `score_event` if the individual experienced the event
+#' - `standardized_score` the value of `score` standardised t obe between -1 and 1
+#' - `rank` the rank of the ordered event and censoring times, with higher numbers indicating
+#'  later times `t_j`
+#'
+#' Data frame containing output from function `find_weights` and also additional columns:
+#' 
 #' @details
 #'
 #' Select which of the three tests to perform using argument `wlr`.
@@ -76,43 +86,19 @@ find_scores<-function(formula,
                              include_cens=TRUE)
   df$score_censored<-with(df,-cumsum(w*(n_event/n_risk)))
   df$score_event<-with(df,score_censored+w)
-  
   #####################################
   ## include treatment group (DM)
 
   formula_vars <- all.vars(formula)
+  time_col <- formula_vars[1]
+  status_col <- formula_vars[2]
   group_col<-formula_vars[-(1:2)]
-  groups <- as.vector(sort(unique(data[[group_col]])))
-  
-  for (i in 1:2){
-    df[[paste0("n_censored_",groups[i])]] <- 
-      rev(diff(rev(c(df[[paste0("n_risk_",groups[i])]],0))) - rev(df[[paste0("n_event_",groups[i])]]))
-  }
-  
-  ###########################################
-  ## replicate rows with ties (DM)
-  df_full <- NULL
-  for (i in seq_along(df$t_j)) {
-    
-    df_i_e_e <- purrr::map_df(rep(i, df[i,"n_event_experimental"]), function(x) df[x,])
-    df_i_e_e$group <- "experimental"
-    df_i_e_e$event <- 1
-    
-    df_i_c_e <- purrr::map_df(rep(i, df[i,"n_event_control"]), function(x) df[x,])
-    df_i_c_e$group <- "control"
-    df_i_c_e$event <- 1
-    
-    df_i_e_c <- purrr::map_df(rep(i, df[i,"n_censored_experimental"]), function(x) df[x,])
-    df_i_e_c$group <- "experimental"
-    df_i_e_c$event <- 0
-    
-    df_i_c_c <- purrr::map_df(rep(i, df[i,"n_censored_control"]), function(x) df[x,])
-    df_i_c_c$group <- "control"
-    df_i_c_c$event <- 0
-    
-    df_full <- rbind(df_full, df_i_e_e, df_i_c_e, df_i_e_c, df_i_c_c)
-  }
-  df <- df_full
+
+  data$t_j<-data[[time_col]]
+  data$event<-data[[status_col]]
+  data$group<-data[[group_col]]
+
+  df<-merge(x = df[,c("t_j","score_censored","score_event")], y = data[,c("t_j","event","group")], by = "t_j", all.x = TRUE)
   ###########################################
   ## pick out score (event or censored) (DM)
   df$score <- with(df, event * score_event + (1 - event) * score_censored)
@@ -136,7 +122,7 @@ find_scores<-function(formula,
 #' @export
 plot.wlrt_score<-function(x,...){
   df<-x$df
-  df$x_pos<-1:nrow(df)
+  df$x_pos<-1:length(unique(df$rank))
   
   df_experimental_cens<-df[df$group == "experimental" & df$event == 0,]
   df_experimental_event<-df[df$group == "experimental" & df$event == 1,]
@@ -153,9 +139,10 @@ plot.wlrt_score<-function(x,...){
   
   axis(side=1, labels=df$rank, at=df$x_pos)
   
-  points(x=df_experimental_cens$x_pos,y=df_experimental_cens$standardized_score,pch=21,bg="white")
-  points(x=df_experimental_event$x_pos,y=df_experimental_event$standardized_score,pch=21,bg="black")
-  points(x=df_control_cens$x_pos,y=df_control_cens$standardized_score,pch=21,bg="white", col = 2)
-  points(x=df_control_event$x_pos,y=df_control_event$standardized_score,pch=21,bg="red", col = 2)
-  
+  mycol_blue <- rgb(0, 0, 255, max = 255, alpha = 100)
+  mycol_red <- rgb(255, 0, 0, max = 255, alpha = 100)
+  points(x=df_control_event$x_pos,y=df_control_event$standardized_score,pch=15,col=mycol_red,cex=1)
+  points(x=df_control_cens$x_pos,y=df_control_cens$standardized_score,pch=0,col=mycol_red,cex=1)
+  points(x=df_experimental_event$x_pos,y=df_experimental_event$standardized_score,pch=16,col=mycol_blue,cex=1)
+  points(x=df_experimental_cens$x_pos,y=df_experimental_cens$standardized_score,pch=1,col=mycol_blue,cex=1)
 }
