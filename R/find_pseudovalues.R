73b74aff031e394440cc093b682e347242e8263a
Barrett <barreis3@glchbs-sp220444.eu.novartis.net>
2022-07-14 12:26:13 +0200

    Move function outside find_pseudovalues function

--- R/find_pseudovalues.R
+++ R/find_pseudovalues.R
@@ -1,58 +1,58 @@
+### rmst1 code from survRM2 package
+rmst1<-function(time, status, tau, alpha=0.05){
+  #-- time
+  #-- statuts
+  #-- tau -- truncation time
+  #-- alpha -- gives (1-alpha) confidence interval
+  
+  ft= survfit(Surv(time, status)~1)
+  idx=ft$time<=tau
+  
+  wk.time=sort(c(ft$time[idx],tau))
+  wk.surv=ft$surv[idx]
+  wk.n.risk =ft$n.risk[idx]
+  wk.n.event=ft$n.event[idx]
+  
+  time.diff <- diff(c(0, wk.time))
+  areas <- time.diff * c(1, wk.surv)
+  rmst = sum(areas)
+  rmst
+  
+  wk.var <- ifelse((wk.n.risk-wk.n.event)==0, 0,
+                   wk.n.event /(wk.n.risk *(wk.n.risk - wk.n.event)))
+  wk.var =c(wk.var,0)
+  rmst.var = sum( cumsum(rev(areas[-1]))^2 * rev(wk.var)[-1])
+  rmst.se  = sqrt(rmst.var)
+  
+  #--- check ---
+  # print(ft, rmean=tau)
+  
+  #--- output ---
+  out=matrix(0,2,4)
+  out[1,]=c(rmst, rmst.se, rmst-qnorm(1-alpha/2)*rmst.se, rmst+qnorm(1-alpha/2)*rmst.se)
+  out[2,]=c(tau-out[1,1], rmst.se, tau-out[1,4], tau-out[1,3])
+  rownames(out)=c("RMST","RMTL")
+  colnames(out)=c("Est.", "se", paste("lower .",round((1-alpha)*100, digits=0), sep=""), 
+                  paste("upper .",round((1-alpha)*100, digits=0), sep=""))
+  
+  Z=list()
+  Z$result=out
+  Z$rmst = out[1,]
+  Z$rmtl = out[2,]
+  Z$tau=tau
+  Z$rmst.var = rmst.var
+  Z$fit=ft
+  class(Z)="rmst1"
+  
+  return(Z)
+  
+}
+
 find_pseudovalues <- function(formula,
                               data,
                               method = "RMST",
                               tau = 1){
-  
-  ##################################################
-  ### rmst1 code from survRM2 package
-  rmst1=function(time, status, tau, alpha=0.05){
-    #-- time
-    #-- statuts
-    #-- tau -- truncation time
-    #-- alpha -- gives (1-alpha) confidence interval
-    
-    ft= survfit(Surv(time, status)~1)
-    idx=ft$time<=tau
-    
-    wk.time=sort(c(ft$time[idx],tau))
-    wk.surv=ft$surv[idx]
-    wk.n.risk =ft$n.risk[idx]
-    wk.n.event=ft$n.event[idx]
-    
-    time.diff <- diff(c(0, wk.time))
-    areas <- time.diff * c(1, wk.surv)
-    rmst = sum(areas)
-    rmst
-    
-    wk.var <- ifelse((wk.n.risk-wk.n.event)==0, 0,
-                     wk.n.event /(wk.n.risk *(wk.n.risk - wk.n.event)))
-    wk.var =c(wk.var,0)
-    rmst.var = sum( cumsum(rev(areas[-1]))^2 * rev(wk.var)[-1])
-    rmst.se  = sqrt(rmst.var)
-    
-    #--- check ---
-    # print(ft, rmean=tau)
-    
-    #--- output ---
-    out=matrix(0,2,4)
-    out[1,]=c(rmst, rmst.se, rmst-qnorm(1-alpha/2)*rmst.se, rmst+qnorm(1-alpha/2)*rmst.se)
-    out[2,]=c(tau-out[1,1], rmst.se, tau-out[1,4], tau-out[1,3])
-    rownames(out)=c("RMST","RMTL")
-    colnames(out)=c("Est.", "se", paste("lower .",round((1-alpha)*100, digits=0), sep=""), paste("upper .",round((1-alpha)*100, digits=0), sep=""))
-    
-    Z=list()
-    Z$result=out
-    Z$rmst = out[1,]
-    Z$rmtl = out[2,]
-    Z$tau=tau
-    Z$rmst.var = rmst.var
-    Z$fit=ft
-    class(Z)="rmst1"
-    
-    return(Z)
-    
-  }
-  #############################################################
+
   ### extract terms from formula
   check_formula(formula=formula,data=data)
   Surv<-survival::Surv
@@ -80,7 +80,6 @@ find_pseudovalues <- function(formula,
   rmst_full <- rmst1(time = data[[time_col]],
                      status = data[[status_col]],
                      tau = tau)$rmst[1]
-  
   n <- length(data[[time_col]])
   
   ### also need the size of each arm...
@@ -88,7 +87,6 @@ find_pseudovalues <- function(formula,
   p_v_rmst <- numeric(length(dat[[time_col]]))
   
   for (i in seq_along(p_v_rmst)){
-    
     data_minus_i <- data[-i,]
     
     rmst_minus_i <- rmst1(time = data_minus_i[[time_col]],
@@ -112,7 +110,4 @@ find_pseudovalues <- function(formula,
   
   
   df_rmst_pseudo
-  
-  
-  
 }
