1f1320749e82223a9abc58eb894e31df00c5dd85
Magirr <magirdo1@glchbs-sp220447.eu.novartis.net>
2022-08-18 15:21:46 +0200

    Remove data.R

--- R/data.R
+++
@@ -1,6 +0,0 @@
-#' Data from the POPLAR study
-#' Biomarker information and outcomes for patients in the POPLAR study
-#'
-#' @format A data frame with 287 rows and 25 variables. 
-#' @source \url{https://www.nature.com/articles/s41591-018-0134-3}
-"poplar"
