18bf5acf060dbd83a1808e6c61c2820108377482
Magirr <magirdo1@glchbs-sp220449.eu.novartis.net>
2022-08-26 11:47:52 +0200

    Update test scores to test data frame rather than plot.

--- tests/testthat/test-find_scores.R
+++ tests/testthat/test-find_scores.R
@@ -32,15 +32,22 @@ df_scores_ms<-find_scores(formula=Surv(event_time,event_status)~group,
                           tau = 4
 )
 
-save_png <- function(code, width = 400, height = 400) {
-  path <- tempfile(fileext = ".png")
-  png(path, width = width, height = height)
-  on.exit(dev.off())
-  code
-  
+
+save_file <- function(code){
+  path <- tempfile(fileext = ".RDS")
+  saveRDS(code,file = path)
   path
 }
 
-expect_snapshot_file(save_png(plot(df_scores_mw)), paste0("plot_mw.png"),cran = TRUE)
-expect_snapshot_file(save_png(plot(df_scores_rmst)), paste0("plot_rmst.png"),cran = TRUE)
-expect_snapshot_file(save_png(plot(df_scores_ms)), paste0("plot_ms.png"),cran = TRUE)
+test_that("scores (mw) example snapshot", {
+  expect_snapshot_file(save_file(df_scores_mw), "df_scores_mw.RDS",cran = TRUE)
+})
+
+test_that("scores (rmst) example snapshot", {
+  expect_snapshot_file(save_file(df_scores_rmst), "df_scores_rmst.RDS",cran = TRUE)
+})
+
+test_that("scores (ms) example snapshot", {
+  expect_snapshot_file(save_file(df_scores_ms), "df_scores_ms.RDS",cran = TRUE)
+})
+
